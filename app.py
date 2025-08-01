4af790bf6febf2451ef2ef26f18ce10cd50a2712
u7434512639-code <u7434512639@gmail.com>
2025-08-01 17:55:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -73,4 +73,9 @@ def ban():
     return jsonify({"status": "not_found"})
 
 if __name__ == "__main__":
-    app.run()
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
